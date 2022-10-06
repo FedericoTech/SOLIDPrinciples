337e51444c0a452e19aa8cfa17188ca34825d817
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-10-06 10:41:39 +0100

    Liskov's Substitution princible

---
+++ 03LiskovSubstitution/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * Liskov's Substitution principle
+ * Parent classes should be easily substituted with their child classes without blowing the application
+ */
+
+
+
+int main()
+{
+	return 0;
+}
